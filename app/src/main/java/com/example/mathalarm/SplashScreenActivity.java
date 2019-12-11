a665d299e2a08d8cc7bb9275e7e486b840bb91df
nadyafitrani <35402211+nadyafitrani@users.noreply.github.com>
2019-12-11 12:28:54 +0700

    Splash Screen Done

--- app/src/main/java/com/example/mathalarm/SplashScreenActivity.java
+++ app/src/main/java/com/example/mathalarm/SplashScreenActivity.java
@@ -2,13 +2,28 @@ package com.example.mathalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
     }
 }
